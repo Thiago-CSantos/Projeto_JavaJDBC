9e9392348f3305a1445447e7c9c69d8c6bc8a0b5
Thiago C. Santos <thiagocarretero433@gmail.com>
2022-12-09 21:12:06 -0300

    Creating project and git repository

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,10 @@
+package aplicacao;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		// TODO Stub de método gerado automaticamente
+
+	}
+
+}
---
+++ src/db/DbExeption.java
@@ -0,0 +1,11 @@
+package db;
+
+public class DbExeption extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public DbExeption(String msgExeption) {
+	super(msgExeption);
+	}
+	
+}
